9fd45154e9a4b59fad11267a5c4df303a0373025
Huy Bui <huy2662004@gmail.com>
2024-10-29 10:19:08 +0700

    fix lỗi like next js

--- app/api/post/like/route.js
+++ app/api/post/like/route.js
@@ -4,30 +4,34 @@ import { currentUser } from '@clerk/nextjs/server';
 
 export const PUT = async (req) => {
   const user = await currentUser();
+
   try {
     await connectToDB();
     const data = await req.json();
+
     if (!user) {
-      return { status: 401, body: 'Unauthorized' };
+      return new Response(JSON.stringify({ message: 'Unauthorized' }), { status: 401 });
     }
+
     const post = await Post.findById(data.postId);
+
     if (post.likes.includes(user.publicMetadata.userMongoId)) {
       const updatedPost = await Post.findByIdAndUpdate(
         data.postId,
         { $pull: { likes: user.publicMetadata.userMongoId } },
         { new: true }
       );
-      return new Response(JSON.stringify(updatedPost), { status: 200 });
+      return new Response(JSON.stringify(updatedPost), { status: 200, headers: { 'Content-Type': 'application/json' } });
     } else {
       const updatedPost = await Post.findByIdAndUpdate(
         data.postId,
         { $addToSet: { likes: user.publicMetadata.userMongoId } },
         { new: true }
       );
-      return new Response(JSON.stringify(updatedPost), { status: 200 });
+      return new Response(JSON.stringify(updatedPost), { status: 200, headers: { 'Content-Type': 'application/json' } });
     }
   } catch (error) {
     console.log('Error liking post:', error);
-    return new Response('Error liking post', { status: 500 });
+    return new Response(JSON.stringify({ message: 'Error liking post' }), { status: 500 });
   }
 };
